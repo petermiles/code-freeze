a83673e6239cf21ce2b91eab3352609ad2234a6b
Pete Kincaid <peter.kincaid@apollo.io>
2023-11-14 11:02:24 -0800

    fix: bump up the time

--- .github/scripts/check-for-codefreeze.js
+++ .github/scripts/check-for-codefreeze.js
@@ -4,7 +4,7 @@ const core = require("@actions/core");
 const codeFreezes = [
   {
     start: "2023-11-14T17:59:19Z",
-    end: "2023-11-14T18:59:19Z",
+    end: "2023-11-14T19:59:19Z",
     reason: "Preparing for major release",
   },
 ];

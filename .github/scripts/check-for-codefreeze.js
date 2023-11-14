fe4d8fd435a04afd7327419d98df046d2f4b7448
Pete Kincaid <peter.kincaid@apollo.io>
2023-11-14 09:59:59 -0800

    fix: try again

---
+++ .github/scripts/check-for-codefreeze.js
@@ -0,0 +1,19 @@
+const codeFreezes = [
+  {
+    start: "2023-11-14T17:59:19Z",
+    end: "2023-11-14T18:59:19Z",
+    reason: "Preparing for major release",
+  },
+];
+
+const currentTime = new Date();
+
+for (const { start, end, reason } of codeFreezes) {
+  const freezeStart = new Date(start);
+  const freezeEnd = new Date(end);
+
+  if (currentTime >= freezeStart && currentTime <= freezeEnd) {
+    console.log(`Code freeze is in effect: ${reason}`);
+    break;
+  }
+}
